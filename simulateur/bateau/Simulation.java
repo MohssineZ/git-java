783078b9c7b321d93450885bd5a3f0dce90074bf
Mohssine Zahri <zahri.mohssine@gmail.com>
2023-02-23 14:34:29 +0100

    modif main ajout constructeur Coque et attacher à un objet Bateau

--- simulateur/bateau/Simulation.java
+++ simulateur/bateau/Simulation.java
@@ -2,23 +2,28 @@ package simulateur.bateau;
 
 public class Simulation {
     public static void main(String[] args) {
-        Bateau monBateau = new Bateau();
-        Bateau monBateau2 = new Bateau();
-        monBateau.setNom("le titanic");
+        Bateau monBateau = new Bateau("titanic");
+        Coque maCoque = new Coque(5);
+        Coque maCoque2 = new Coque(3);
+        monBateau.setcoque(maCoque);
+        System.out.println(
+                "points de vie de mon bateau " + monBateau.getNom() + " : " + monBateau.getcoque().getPointDeVie());
         monBateau.prendLaMer();
         monBateau.navigue(500);
-        monBateau.navigue(120);
-        // monBateau.arriveAuPort();
         monBateau.prendLaMer();
         monBateau.navigue(120);
         monBateau.arriveAuPort();
-        monBateau2.setNom("le titanic 2");
+        Bateau monBateau2 = new Bateau("titanic2");
+        monBateau2.setcoque(maCoque2);
+        System.out.println(
+                "points de vie de mon bateau " + monBateau2.getNom() + " : " + monBateau2.getcoque().getPointDeVie());
         monBateau2.prendLaMer();
         monBateau2.navigue(500);
         monBateau2.arriveAuPort();
         monBateau2.prendLaMer();
         monBateau2.navigue(120);
         monBateau2.arriveAuPort();
+        System.out.println("nombre de bateau construit: " + Bateau.getNombreBateauConstruits());
 
     }
 }
