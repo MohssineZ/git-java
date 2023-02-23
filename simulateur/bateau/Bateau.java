24ab9aeefaf4b825f732bce7879cf253edc7ceed
Mohssine Zahri <zahri.mohssine@gmail.com>
2023-02-23 14:30:29 +0100

    AJOUT OBJET COQUE ET MODIF NOM EN FINAL

--- simulateur/bateau/Bateau.java
+++ simulateur/bateau/Bateau.java
@@ -2,9 +2,15 @@ package simulateur.bateau;
 
 public class Bateau {
 
-    private String nom;
+    private final String nom;
     private double milesParcourus;
     private static int nombreBateauConstruits;
+    private Coque coque;
+
+    public Bateau(String nom) {
+        this.nom = nom;
+        nombreBateauConstruits++;
+    }
 
     public static int getNombreBateauConstruits() {
         return nombreBateauConstruits;
@@ -14,22 +20,26 @@ public class Bateau {
         return nom;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
     public boolean prendLaMer() {
         return true;
     }
 
     public void navigue(double miles) {
-        System.out.println("bateau navigue " + nom);
+        System.out.println(this.nom + " navigue ");
         milesParcourus = milesParcourus + miles;
     }
 
     public void arriveAuPort() {
-        System.out.println("bateau arrive au port et nombre de miles parcourus est " + milesParcourus);
+        System.out.println(this.nom + " arrive au port et nombre de miles parcourus est " + milesParcourus);
         milesParcourus = 0;
+    }
+
+    public Coque getcoque() {
+        return coque;
+    }
+
+    public void setcoque(Coque coque) {
+        this.coque = coque;
     }
 
 }
